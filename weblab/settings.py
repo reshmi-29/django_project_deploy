83b7bebb1b162e05d995e1f64678801945c43e5d
reshmi-29 <nareshmi1003@gmail.com>
2025-04-16 00:04:41 +0600

    css added

--- weblab/settings.py
+++ weblab/settings.py
@@ -137,7 +137,3 @@ LOGIN_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@edulearn.com'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
