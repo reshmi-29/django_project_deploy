5653eeb0374b3c3a531ca923d4e212b4758a13ac
reshmi-29 <nareshmi1003@gmail.com>
2025-04-16 12:14:24 +0600

    please Allah

--- weblab/settings_rail.py
+++ weblab/settings_rail.py
@@ -4,7 +4,8 @@ from decouple import config
 SECRET_KEY= config('SECRET_KEY')
 
 ALLOWED_HOSTS = [ 'web-production-42e7a.up.railway.app']
-CSRF_TRUSTED_ORIGINS=['https://web-production-42e7a.up.railway.app/'] 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-42e7a.up.railway.app']
+ 
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
